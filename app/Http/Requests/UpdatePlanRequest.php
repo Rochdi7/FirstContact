dd140a333a6c82a00952a5d8e4b9f866449d371f
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-05-19 20:17:45 +0100

    add requests files

---
+++ app/Http/Requests/UpdatePlanRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePlanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->user()->can('edit plans');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                'unique:plans,name,' . $this->route('plan')->id
+            ],
+            'max_templates' => [
+                'required',
+                'integer',
+                'min:1',
+            ],
+            'ai_enabled' => [
+                'required',
+                'boolean',
+            ],
+            'price' => [
+                'required',
+                'numeric',
+                'min:0',
+            ],
+            'features' => [
+                'nullable',
+                'string'
+            ],
+        ];
+    }
+}
