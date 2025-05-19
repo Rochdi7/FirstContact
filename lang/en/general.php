32ebb3ae5b369661fe2c3824d4b4c877376321f9
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-05-19 20:19:19 +0100

    add file plan.php for translation english

--- lang/en/general.php
+++ lang/en/general.php
@@ -3,6 +3,7 @@
 return [
     'welcome_message' => 'Optimize Costs, Maximize Profits',
     'crud' => [
+        'actions' => 'Actions',
         'created_at' => 'Created At',
         'updated_at' => 'Updated At',
     ],
---
+++ lang/en/plans.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'title' => 'Plan Management',
+    'create' => 'Create Plan',
+    'edit' => 'Edit Plan',
+    'new' => 'New Plan',
+    'show' => 'Show Plan: :name',
+    'fields' => [
+        'name' => 'Name',
+        'max_templates' => 'Max Templates',
+        'ai_enabled' => 'AI Enabled',
+        'price' => 'Price',
+        'features' => 'Features',
+    ],
+    'filter' => [
+        'name' => 'Filter by name',
+        'max_templates' => 'Filter by max templates',
+        'price' => 'Filter by price',
+    ],
+];
