a56f4dc2de15fc333104d535c753e3c6b03aa04b
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-05-28 19:05:06 +0100

    add migrations for templates, messages, message templates, mail providers, contacts, and recipients

---
+++ database/migrations/2025_05_28_185928_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('template_id')->nullable()->constrained('templates')->onDelete('set null');
+            $table->foreignId('message_template_id')->nullable()->constrained('message_templates')->onDelete('set null');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('mail_provider_id')->constrained()->onDelete('cascade');
+            $table->timestamp('created_at')->useCurrent();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
