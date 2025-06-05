0f9425eab16ff070ec54dc2ccf40ce6a90daf8ea
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-06-05 17:17:02 +0100

    update path of MailProvider policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use App\Models\MailProvider;
 use App\Policies\MailProviderPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
