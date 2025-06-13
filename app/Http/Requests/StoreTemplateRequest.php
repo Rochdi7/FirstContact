09d18f44d90d878037d1c18eb9f44ba597ec581c
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-06-13 18:34:13 +0100

    Add Store & Update TemplateRequest for validating Template creation

---
+++ app/Http/Requests/StoreTemplateRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTemplateRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return auth()->user()->can('create templates');
+    }
+
+    public function rules(): array
+    {
+        return [
+            'plan_id' => ['required', 'exists:plans,id'],
+            'name' => ['required', 'string', 'max:255'],
+            'view_path' => ['required', 'string', 'max:255'],
+        ];
+    }
+}
