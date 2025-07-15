a6c838b77c1bf9b423f55ee20feae93ddb875818
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-07-16 00:19:17 +0200

    add route api

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ContactController;
+use App\Http\Controllers\Api\MailProviderController;
+use App\Http\Controllers\Api\MessageTemplateController;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group
+| assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// PUBLIC ROUTES
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 
+// AUTHENTICATED ROUTES
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::apiResource('contacts', ContactController::class);
+    Route::apiResource('mail-providers', MailProviderController::class);
+    Route::apiResource('message-templates', MessageTemplateController::class);
+
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::put('/profile', [AuthController::class, 'updateProfile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
