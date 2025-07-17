0043ab133d9048d7ce772ab15b5a63b38d6eba1e
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-07-17 01:50:52 +0100

    update api route

--- routes/api.php
+++ routes/api.php
@@ -5,17 +5,8 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ContactController;
 use App\Http\Controllers\Api\MailProviderController;
 use App\Http\Controllers\Api\MessageTemplateController;
+use App\Http\Controllers\Api\TemplateController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application.
-| These routes are loaded by the RouteServiceProvider within a group
-| assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 // PUBLIC ROUTES
 Route::post('/login', [AuthController::class, 'login']);
@@ -27,6 +18,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('contacts', ContactController::class);
     Route::apiResource('mail-providers', MailProviderController::class);
     Route::apiResource('message-templates', MessageTemplateController::class);
+
+    // Only listing templates for customers
+    Route::get('templates', [TemplateController::class, 'index']);
 
     Route::get('/user', [AuthController::class, 'user']);
     Route::put('/profile', [AuthController::class, 'updateProfile']);
