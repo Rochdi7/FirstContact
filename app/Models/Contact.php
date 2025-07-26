c36694c684969a3068c3f03bf2ee91b451715643
Rochdi Karouali <rochdi.karouali1234@gmail.com>
2025-07-26 20:07:42 +0100

    update contact add media library for mobile image api

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Contact extends Model
+class Contact extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
     protected $fillable = ['user_id', 'first_name', 'last_name', 'company', 'email', 'phone'];
 
     public function user()
